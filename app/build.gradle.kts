8cdff41696d18b09a2756dc6f9e3f2d82511d01a
ndev-hoster <ndev.hoster@gmail.com>
2026-09-14 00:13:34 +0530

    chore: bump version to 0.1.0-pre6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.ndev.moodyroutine"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.0-pre5"
+        versionCode = 6
+        versionName = "0.1.0-pre6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
